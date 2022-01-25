14b9bb8eba7cd6d50c9b859fc87e800f0fe698f4
2gyome <nsk324@naver.com>
2022-01-26 01:04:03 +0900

    [학습][220125]

---
+++ 11_20.py
@@ -0,0 +1,52 @@
+#11
+삼성전자 = 50000
+총평가금액 = 삼성전자 *10
+print(총평가금액)
+
+#12
+시가총액 = 2980000000000
+현재가 = 5000
+per = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(per, type(per))
+
+#13 문자열 출력하기 hello! python )
+s = "hello"
+t = "python"
+print(s,"!",t)
+print(s+"!",t) #이거임
+
+#14
+print(2+2*3)
+
+#15
+a = 128
+print(type(a))
+a = "128"
+print(type(a))
+
+#16 정수 100을 문자로 변경하기
+num = 100
+result = str(100)
+print(result, type(result))
+
+#17 문자열을 정수형으로 변환
+num_str = "720"
+result = int(num_str)
+print(result, type(result))
+
+#18 문자열을 실수로 변환
+str_num = "15.79"
+result = float(str_num)
+print(result, type(result))
+
+#19 문자열을 정수로 변환
+year = "2020"
+num_year = int(year)
+print(num_year-1, num_year, num_year+1)
+
+#20 에어컨 이자
+월 = 48584
+총액 = 월 * 36
+print(총액)
---
+++ 1_10.py
@@ -0,0 +1,35 @@
+print("맥 실행 단축키는 ctrl + r")
+print("맥 디버그 단축키는 ctrl + d")
+
+#1
+print("Hello World")
+
+#2
+print("Mary's cosmetics")
+
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+
+#4.
+print('"C:\Windows"')
+
+#5. \n은 개행 \t는 탭
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+#6. "" , 해두 한줄로 출력됨
+print("오늘은", "일요일")
+
+#7.sep 사용
+print("naver;kakao;samsung")
+print("naver", "kakao", "samsung", sep=";")
+
+#8. end 사용
+print("naver/kakao/sk/samsung")
+print("naver", "kakao", "samsung", sep="/")
+
+
+#9 print 함수 두 번 사용하여 줄 바꿈 없이 출력하기
+print("first", end=''); print("second")
+
+#10
+print(5/3)
